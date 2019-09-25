c58de051beb6dccce0718c7be8e4c10ab9f69bf9
Soroosh Sarabadani <soroosh.sarabadani@gmail.com>
2019-09-25 12:54:51 +0200

    add a simple controller

--- src/main/java/com/softwaretalks/jangul/JanguleServiceApplication.java
+++ src/main/java/com/softwaretalks/jangul/JanguleServiceApplication.java
@@ -2,12 +2,21 @@ package com.softwaretalks.jangul;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 public class JanguleServiceApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(JanguleServiceApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(JanguleServiceApplication.class, args);
+    }
 }
+
+@RestController
+class HiController {
+    @GetMapping("/")
+    public String hello() {
+        return "Hello Softwaretalks";
+    }
+}
+
