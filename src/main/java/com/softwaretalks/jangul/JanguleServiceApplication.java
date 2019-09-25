bb8c32358cb07e9706c5208224b9be8c0c2e4966
Soroosh Sarabadani <soroosh.sarabadani@gmail.com>
2019-09-25 12:54:45 +0200

    setup project skeleton

---
+++ src/main/java/com/softwaretalks/jangul/JanguleServiceApplication.java
@@ -0,0 +1,13 @@
+package com.softwaretalks.jangul;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JanguleServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JanguleServiceApplication.class, args);
+	}
+
+}
